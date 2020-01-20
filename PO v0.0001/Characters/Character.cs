be29cb17b008535d6ab8981105e5f92742eb21bc
norbertszsor <47736350+norbertszsor@users.noreply.github.com>
2020-01-20 21:22:03 +0100

    Versus

--- PO v0.0001/Characters/Character.cs
+++ PO v0.0001/Characters/Character.cs
@@ -19,45 +19,8 @@
             this.nick = nick;
         }
 
-        public void Add_expirience(double pending_exp,Character character)
-        {
-            this.expirience = this.expirience + pending_exp;
-            if(this.expirience>this.lvl*100)
-            {
-                this.Lvl_up(character);
-            }
-        }
+      
 
-        public void Lvl_up(Character character)
-        {
-            if(character is Mage)
-            {
-                this.inteligence = this.inteligence + this.inteligence * 0.40;
-                this.strength = this.strength + this.strength * 0.10;
-                this.dexterity = this.dexterity + this.dexterity * 0.10;
-                this.hit_points = this.hit_points + this.strength * 0.10;
-                this.mana = this.mana + this.inteligence * 0.10;
-               
-            }
-            else if(character is Rouge)
-            {
-                this.inteligence = this.inteligence + this.inteligence * 0.10;
-                this.strength = this.strength + this.strength * 0.10;
-                this.dexterity = this.dexterity + this.dexterity * 0.40;
-                this.hit_points = this.hit_points + this.strength * 0.10;
-                this.mana = this.mana + this.inteligence * 0.10;
-
-            }
-            else if(character is Warrior)
-            {
-                this.inteligence = this.inteligence + this.inteligence * 0.10;
-                this.strength = this.strength + this.strength * 0.40;
-                this.dexterity = this.dexterity + this.dexterity * 0.10;
-                this.hit_points = this.hit_points + this.strength * 0.10;
-                this.mana = this.mana + this.inteligence * 0.10;
-
-            }
-
-        }
+       
     }
 }
